841bc8c4738aea9384a90ffacd22cdcbdc8784c9
Enosk <44186843+Nokking@users.noreply.github.com>
2023-02-02 18:53:00 +0900

    Update server.py
    CO

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
-from flask import Flask
-from threading import Thread
+# from flask import Flask
+# from threading import Thread
 
-app = Flask("")
+# app = Flask("")
 
-@app.route("/")
-def main():
-  return "AZR is alive!"
+# @app.route("/")
+# def main():
+#   return "AZR is alive!"
 
-def run():
-  app.run("0.0.0.0", port=8080)
+# def run():
+#   app.run("0.0.0.0", port=8080)
 
-def keep_alive():
-  t = Thread(target=run)
-  t.start()
+# def keep_alive():
+#   t = Thread(target=run)
+#   t.start()
